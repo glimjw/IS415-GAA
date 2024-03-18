2b733028ce5a13c352f78f285e1a2ae74bc8e045
glimjw <geraldlimjw@gmail.com>
2024-03-18 10:33:25 +0800

    In class 9

--- Take-home_Ex/Take-home_Ex03/HawkerCtrDistribution/app.R
+++ Take-home_Ex/Take-home_Ex03/HawkerCtrDistribution/app.R
@@ -1,10 +1,35 @@
-library(shiny)
+pacman::p_load(shiny, maptools, sf, raster, spatstat, tmap, readr)
+
+mpsz_sf <- st_read(dsn = "data/geospatial", 
+                   layer = "MP14_SUBZONE_WEB_PL")
+
+hawker_centre_sf <- read_csv("data/aspatial/updated_hawker_centres.csv")
+hawker_centre_sf <- st_as_sf(hawker_centre_sf, coords = c("Longitude", "Latitude"), crs = 4326)
+
+sg_sf <- st_read(dsn = "data/", 
+                 layer = "CostalOutline")
+
+target_crs = st_crs(sg_sf)
+hawker_centre_sf <- st_transform(hawker_centre_sf, target_crs)
+
+mpsz <- as_Spatial(mpsz_sf)
+hawker_centre <- as_Spatial(hawker_centre_sf)
+sg <- as_Spatial(sg_sf)
+
+hawker_centre_sp <- as(hawker_centre, "SpatialPoints")
+sg_sp <- as(sg, "SpatialPolygons")
+
+hawker_centre_ppp <- as.ppp(hawker_centre_sp)
+
+hawker_centre_SG_ppp_km <- rescale(hawker_centre_SG_ppp, 1000, "km")
+
+sg_owin <- as(sg_sp, "owin")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Kernel Density"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
